945afe894bfb35fc83c2ec5782eae08a80744f15
PathakTushar <tusharph1@gmail.com>
2024-08-12 09:18:11 +0530

    changes

--- server/app.js
+++ server/app.js
@@ -26,8 +26,13 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:4173", process.env.CLIENT_URL],
-  credentials: true
+    origin: [
+        "http://localhost:5173",
+        "http://localhost:4173",
+        process.env.CLIENT_URL,
+      ],
+      methods: ["GET", "POST", "PUT", "DELETE"],
+      credentials: true,
 }))
 
 app.get('/cuisines', async (req, res) => {
